37dc9d2c814d58981846848fedf54cba44dfa326
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-11 12:13:18 +0300

    Implemented class LibrarySystemEfDbContextSaveChanges

---
+++ LibrarySystem/Data/LibrarySystem.Data/Repositories/LibrarySystemEfDbContextSaveChanges.cs
@@ -0,0 +1,26 @@
+﻿using Bytes2you.Validation;
+using LibrarySystem.Data.Contracts;
+
+namespace LibrarySystem.Data.Repositories
+{
+    public class LibrarySystemEfDbContextSaveChanges : ILibrarySystemEfDbContextSaveChanges
+    {
+        private readonly ILibrarySystemEfDbContext context;
+
+        public LibrarySystemEfDbContextSaveChanges(ILibrarySystemEfDbContext context)
+        {
+            Guard.WhenArgument(context, "context").IsNull().Throw();
+
+            this.context = context;
+        }
+
+        public int SaveChanges()
+        {
+            return this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
