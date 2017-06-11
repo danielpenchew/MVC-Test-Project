26b0303ab219588cb591351ba10e70711c9400da
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-11 12:06:29 +0300

    Created interface ILibrarySystemEfDataProvider

---
+++ LibrarySystem/Data/LibrarySystem.Data/Contracts/ILibrarySystemEfDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LibrarySystem.Data.Contracts
+{
+    public interface ILibrarySystemEfDataProvider<T> : IDisposable where T : class
+    {
+        IQueryable<T> All();
+
+        void Add(T entity);
+
+        T GetById(Guid id);
+
+        void Delete(Guid id);
+
+        void Delete(T entity);
+
+        void Update(T entity);
+    }
+}
