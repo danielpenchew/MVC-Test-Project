39b0d3f87dad835773d1fe4561170276ac74c291
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-12 14:19:32 +0300

    Added tests
    Added tests for SaveChanges and DbContext

---
+++ LibrarySystem/Tests/LibrarySystem.Data.Tests/LibrarySystemDbContextTests/LibrarySystemEfDbContextTests.cs
@@ -0,0 +1,54 @@
+﻿using LibrarySystem.Data.Contracts;
+using LibrarySytem.Data.Models.Models;
+using NUnit.Framework;
+using System.Data.Entity;
+
+namespace LibrarySystem.Data.Tests.LibrarySystemDbContextTests
+{
+    [TestFixture]
+    public class LibrarySystemEfDbContextTests
+    {
+        [Test]
+        public void Constructor_Should_HaveParameterlessConstructor()
+        {
+            // Arrange & Act
+            var context = new LibrarySystemEfDbContext();
+
+            // Assert
+            Assert.IsInstanceOf<LibrarySystemEfDbContext>(context);
+        }
+
+        [Test]
+        public void Authors_ShouldBeInstanceOfIDbSet()
+        {
+            // Arrange & Act
+            var context = new LibrarySystemEfDbContext();
+            var authors = context.Authors;
+
+            // Assert
+            Assert.IsInstanceOf<IDbSet<Author>>(authors);
+        }
+
+        [Test]
+        public void Books_ShouldBeInstanceOfIDbSet()
+        {
+            // Arrange & Act
+            var context = new LibrarySystemEfDbContext();
+            var books = context.Books;
+
+            // Assert
+            Assert.IsInstanceOf<IDbSet<Book>>(books);
+        }
+
+        [Test]
+        public void Create_Should_ReturnNewDbContextInstance()
+        {
+            // Arrange & Act
+            var newContext = LibrarySystemEfDbContext.Create();
+
+            // Assert
+            Assert.IsNotNull(newContext);
+            Assert.IsInstanceOf<ILibrarySystemEfDbContext>(newContext);
+        }
+    }
+}
---
+++ LibrarySystem/Tests/LibrarySystem.Data.Tests/LibrarySystemEfDbContextSaveChangesTests/LibrarySystemEfDbContextSaveChangesTests.cs
@@ -0,0 +1,53 @@
+﻿using LibrarySystem.Data.Contracts;
+using LibrarySystem.Data.Repositories;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Data.Tests.LibrarySystemEfDbContextSaveChangesTests
+{
+    [TestFixture]
+    public class LibrarySystemEfDbContextSaveChangesTests
+    {
+        [Test]
+        public void Constructor_ShouldThrowArgumentNullException_WhenContextIsNull()
+        {
+            // Arrange
+            ILibrarySystemEfDbContext context = null;
+
+            // Act & Assert
+            Assert.That(
+                () => new LibrarySystemEfDbContextSaveChanges(context),
+                Throws.InstanceOf<ArgumentNullException>().With.Message.Contains("context"));
+        }
+
+        [Test]
+        public void Constructor_ShouldNotThrow_WhenContextIsNotNull()
+        {
+            // Arrange
+            var contextMock = new Mock<ILibrarySystemEfDbContext>();
+            var librarySytemEfDbContextSaveChanges = new LibrarySystemEfDbContextSaveChanges(contextMock.Object);
+
+            // Act & Assert
+            Assert.That(librarySytemEfDbContextSaveChanges, Is.Not.Null);
+        }
+
+        [Test]
+        public void SaveChanges_ShouldBeCalledOnce()
+        {
+            // Arrange
+            var contextMock = new Mock<ILibrarySystemEfDbContext>();
+            var contextSaveChanges = new LibrarySystemEfDbContextSaveChanges(contextMock.Object);
+
+            // Act
+            contextSaveChanges.SaveChanges();
+
+            // Assert
+            contextMock.Verify(c => c.SaveChanges(), Times.Once);
+        }
+    }
+}
