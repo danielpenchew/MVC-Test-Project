69da1b6a31f7c00eee5ec64c2ef8e58943adbe7e
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-12 14:53:22 +0300

    Added test for LibrarySystemEfDataProvider

--- LibrarySystem/Tests/LibrarySystem.Data.Tests/LibrarySystemDataProviderTests/AddShould.cs
+++ LibrarySystem/Tests/LibrarySystem.Data.Tests/LibrarySystemDataProviderTests/AddShould.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LibrarySystem.Data.Contracts;
+using LibrarySystem.Data.Repositories;
+using LibrarySytem.Data.Models.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Runtime.Serialization;
 
 namespace LibrarySystem.Data.Tests.LibrarySystemDataProviderTests
 {
-    class AddShould
+    [TestFixture]
+    public class AddShould
     {
+        [Test]
+        public void Throw_NullReferenceException_WhenPassedArgumentIsNull()
+        {
+            // Arrange
+            var contextMock = new Mock<ILibrarySystemEfDbContext>();
+            var setMock = new Mock<DbSet<IBook>>();
+
+            // Act
+            contextMock.Setup(set => set.Set<IBook>()).Returns(setMock.Object);
+            var dbSetMock = new LibrarySystemEfDataProvider<IBook>(contextMock.Object);
+            IBook entity = null;
+
+            // Assert
+            Assert.That(() => dbSetMock.Add(entity),
+                Throws.InstanceOf<NullReferenceException>());
+        }
     }
 }
