d18ef9f498a428a886d4920a8db0e0d8030b4700
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-12 14:59:04 +0300

    Added test
    Method All from LibrarySystemEfDataProvider 100% covered

---
+++ LibrarySystem/Tests/LibrarySystem.Data.Tests/LibrarySystemDataProviderTests/AllShould.cs
@@ -0,0 +1,30 @@
+﻿using LibrarySystem.Data.Contracts;
+using LibrarySystem.Data.Repositories;
+using LibrarySytem.Data.Models.Contracts;
+using Moq;
+using NUnit.Framework;
+using System.Data.Entity;
+
+namespace LibrarySystem.Data.Tests.LibrarySystemDataProviderTests
+{
+    [TestFixture]
+    public class AllShould
+    {
+        [Test]
+        public void ReturnEntitiesOfGivenSet()
+        {
+            // Arrange
+            var dbContextMock = new Mock<ILibrarySystemEfDbContext>();
+            var setMock = new Mock<DbSet<IBook>>();
+
+            // Act
+            dbContextMock.Setup(x => x.Set<IBook>()).Returns(setMock.Object);
+            var dbSetMock = new LibrarySystemEfDataProvider<IBook>(dbContextMock.Object);
+
+            // Assert
+            Assert.IsNotNull(dbSetMock.All());
+            Assert.IsInstanceOf(typeof(DbSet<IBook>), dbSetMock.All());
+            Assert.AreSame(dbSetMock.All(), dbSetMock.DbSet);
+        }
+    }
+}
