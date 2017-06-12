6daa5d3f5e81dd95fa54e3bdd7a445b942b5d90a
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-12 17:40:29 +0300

    Implemented AuthorServices

---
+++ LibrarySystem/Services/LibrarySystem.Data.Services/AuthorServices.cs
@@ -0,0 +1,31 @@
+﻿using LibrarySystem.Data.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibrarySytem.Data.Models.Models;
+using LibrarySystem.Data.Contracts;
+
+namespace LibrarySystem.Data.Services
+{
+    public class AuthorServices : IAuthorServices
+    {
+        private readonly ILibrarySystemEfDataProvider<Author> authorDataProvider;
+
+        public AuthorServices(ILibrarySystemEfDataProvider<Author> authorDataProvider)
+        {
+            Guard
+        }
+
+        public IQueryable<Author> GetAllAuthors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
