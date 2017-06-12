b5396e4200159f8ddfd57fb78755550edf5c7155
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-13 00:23:19 +0300

    GetById method from BookService class - 100% covered

---
+++ LibrarySystem/Tests/LibrarySystem.Data.Services.Tests/BookServicesTests/GetById_Should.cs
@@ -0,0 +1,48 @@
+﻿using LibrarySystem.Data.Contracts;
+using LibrarySytem.Data.Models.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Data.Services.Tests.BookServicesTests
+{
+    [TestFixture]
+    public class GetById_Should
+    {
+        [Test]
+        public void BeCalledOnce_IfParametersAreValid()
+        {
+            // Arrange
+            var dbSetMock = new Mock<ILibrarySystemEfWrapper<Book>>();
+            var saveChangesMock = new Mock<ILibrarySystemEfDbContextSaveChanges>();
+            var bookServices = new BookServices(dbSetMock.Object, saveChangesMock.Object);
+            var bookMock = new Mock<Book>();
+
+            // Act
+            bookServices.GetById(bookMock.Object.Id);
+
+            // Assert
+            dbSetMock.Verify(r => r.GetById(bookMock.Object.Id), Times.Once);
+        }
+
+        [Test]
+        public void ReturnTheProperBookWithId_IfCalled()
+        {
+            // Arrange
+            var dbSetMock = new Mock<ILibrarySystemEfWrapper<Book>>();
+            var saveChangesMock = new Mock<ILibrarySystemEfDbContextSaveChanges>();
+            var bookServices = new BookServices(dbSetMock.Object, saveChangesMock.Object);
+            var bookMock = new Mock<Book>();
+
+            // Act
+            dbSetMock.Setup(r => r.GetById(bookMock.Object.Id)).Returns(() => bookMock.Object);
+
+            // Assert
+            Assert.IsInstanceOf<Book>(bookServices.GetById(bookMock.Object.Id));
+        }
+    }
+}
