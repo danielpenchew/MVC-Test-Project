ca3b548348a3552de3c81d438f79dfd4535fa155
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-12 17:36:03 +0300

    Create Services project
     and added IAuthorServices and IBook Services...

---
+++ LibrarySystem/Services/LibrarySystem.Data.Services/Contracts/IAuthorServices.cs
@@ -0,0 +1,13 @@
+﻿using LibrarySytem.Data.Models.Models;
+using System;
+using System.Linq;
+
+namespace LibrarySystem.Data.Services.Contracts
+{
+    public interface IAuthorServices
+    {
+        IQueryable<Author> GetAllAuthors();
+
+        Author GetById(Guid id);
+    }
+}
---
+++ LibrarySystem/Services/LibrarySystem.Data.Services/Contracts/IBookServices.cs
@@ -0,0 +1,20 @@
+﻿using LibrarySytem.Data.Models.Models;
+using System;
+
+namespace LibrarySystem.Data.Services.Contracts
+{
+    public interface IBookServices
+    {
+        void AddBook(Book book);
+
+        void DeleteBook(Book book);
+
+        void DeleteBookById(Guid id);
+
+        Book GetById(Guid id);
+
+        void CreateBook(Book book);
+
+        void UpdateBook(Book book);
+    }
+}
