f257c2881a071778335eedf87f166e7ff37ea115
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-12 16:42:56 +0300

    Added test for Update method in class LibrarySystemEfDataProvider

---
+++ LibrarySystem/Tests/LibrarySystem.Data.Tests/LibrarySystemDataProviderTests/Update_Should.cs
@@ -0,0 +1,31 @@
+﻿using LibrarySystem.Data.Contracts;
+using LibrarySystem.Data.Repositories;
+using LibrarySytem.Data.Models.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Data.Entity;
+
+namespace LibrarySystem.Data.Tests.LibrarySystemDataProviderTests
+{
+    [TestFixture]
+    public class Update_Should
+    {
+        [Test]
+        public void Throw_NullReferenceException_WhenPassetParameterIsNull()
+        {
+            // Arrange
+            var dbContextMock = new Mock<ILibrarySystemEfDbContext>();
+            var setMock = new Mock<DbSet<IBook>>();
+
+            // Act
+            dbContextMock.Setup(x => x.Set<IBook>()).Returns(setMock.Object);
+            var dbSetMock = new LibrarySystemEfDataProvider<IBook>(dbContextMock.Object);
+            IBook entity = null;
+
+            // Assert
+            Assert.That(() => dbSetMock.Update(entity),
+                Throws.InstanceOf<NullReferenceException>());
+        }
+    }
+}
