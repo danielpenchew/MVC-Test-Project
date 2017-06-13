50bb019c7faf7366eacfde3fc9fdbeae7a6adaeb
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-13 13:30:24 +0300

    Method GetById from AuthorServices - 100% covered

---
+++ LibrarySystem/Tests/LibrarySystem.Data.Services.Tests/AuthorServicesTests/GetById_Should.cs
@@ -0,0 +1,26 @@
+﻿using LibrarySystem.Data.Contracts;
+using LibrarySytem.Data.Models.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace LibrarySystem.Data.Services.Tests.AuthorServicesTests
+{
+    [TestFixture]
+    public class GetById_Should
+    {
+        [Test]
+        public void ReturnCorrectAuthor_WhenIdIsCorrect()
+        {
+            // Arrange
+            var dbSetMock = new Mock<ILibrarySystemEfWrapper<Author>>();
+            var authorServices = new AuthorServices(dbSetMock.Object);
+            var authorWithId = new Mock<Author>();
+
+            // Act
+            authorServices.GetById(authorWithId.Object.Id);
+
+            // Assert
+            dbSetMock.Verify(r => r.GetById(authorWithId.Object.Id), Times.Once);
+        }
+    }
+}
