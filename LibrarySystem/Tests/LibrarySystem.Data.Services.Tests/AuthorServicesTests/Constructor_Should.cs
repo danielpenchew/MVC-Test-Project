f7f2a42f728b54e1a2ba6a874067b9025e0d2848
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-13 13:18:55 +0300

    AuthorServices constructor - 100% covered with unit tests

---
+++ LibrarySystem/Tests/LibrarySystem.Data.Services.Tests/AuthorServicesTests/Constructor_Should.cs
@@ -0,0 +1,22 @@
+﻿using LibrarySystem.Data.Contracts;
+using LibrarySytem.Data.Models.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace LibrarySystem.Data.Services.Tests.AuthorServicesTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void CreateInstanceOfAuthorServices_IfParametersAreValid()
+        {
+            // Arrange & Act
+            var dbSetMock = new Mock<ILibrarySystemEfWrapper<Author>>();
+            var authorServices = new AuthorServices(dbSetMock.Object);
+
+            // Assert
+            Assert.That(authorServices, Is.Not.Null.And.InstanceOf<AuthorServices>());
+        }
+    }
+}
