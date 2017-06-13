46434edfbc9466a4f9f09382825b8048ff5827d9
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-13 13:25:02 +0300

    Method GetAllAuthors from AuthorServices - 100% covered

---
+++ LibrarySystem/Tests/LibrarySystem.Data.Services.Tests/AuthorServicesTests/GetAllAuthors_Should.cs
@@ -0,0 +1,28 @@
+﻿using LibrarySystem.Data.Contracts;
+using LibrarySytem.Data.Models.Models;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibrarySystem.Data.Services.Tests.AuthorServicesTests
+{
+    [TestFixture]
+    public class GetAllAuthors_Should
+    {
+        [Test]
+        public void ReturnInstanceOfIQueryable_IfCalled()
+        {
+            // Arrange
+            var dbSetMock = new Mock<ILibrarySystemEfWrapper<Author>>();
+            var authorServices = new AuthorServices(dbSetMock.Object);
+
+            // Act
+            IEnumerable<Author> expectedResult = new List<Author>() { new Author(), new Author(), new Author() };
+            dbSetMock.Setup(r => r.All()).Returns(() => expectedResult.AsQueryable());
+
+            // Assert
+            Assert.IsInstanceOf<IQueryable<Author>>(authorServices.GetAllAuthors());
+        }
+    }
+}
