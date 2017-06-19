7d110900aba316244703627b8bc75886193b3543
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-19 23:44:28 +0300

    Models - 94% covered with unit tests

---
+++ LibrarySystem/Tests/LibrarySystem.Data.Models.Tests/UserTests/UserTests.cs
@@ -0,0 +1,21 @@
+﻿using LibrarySytem.Data.Models.Models;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LibrarySystem.Data.Models.Tests.UserTests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        [Test]
+        public void Books_ShouldGetAndSetDataCorrectly()
+        {
+            // Arrange & Act
+            HashSet<Book> books = new HashSet<Book>();
+            User user = new User() { Books = books };
+
+            // Assert
+            Assert.AreSame(user.Books, books);
+        }
+    }
+}
