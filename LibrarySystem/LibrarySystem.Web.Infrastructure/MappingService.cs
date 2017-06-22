2aaa3c69c777003e155d69a6213446bce99ac433
Daniel Penchev <daniel.penchew@gmail.com>
2017-06-22 16:39:19 +0300

    Added automapper config file and MappinService

---
+++ LibrarySystem/LibrarySystem.Web.Infrastructure/MappingService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+using LibrarySystem.Web.Infrastucture.Contracts;
+
+namespace LibrarySystem.Web.Infrastucture
+{
+    public class MappingService : IMappingService
+    {
+        public TDestination Map<TDestination>(object source)
+        {
+            return Mapper.Map<TDestination>(source);
+        }
+
+        public TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
+        {
+            return Mapper.Map<TSource, TDestination>(source, destination);
+        }
+    }
+}
